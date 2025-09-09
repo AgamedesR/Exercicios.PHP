d606c61c7049d7c4dc7a6aa042c5fce12a223b8a
Agamedes <agamedesrmeira@gmail.com>
2025-09-09 11:21:36 -0300

    aula 5

---
+++ Aulas 4-5/Exemplo 6/escrever_arquivo.php
@@ -0,0 +1,15 @@
+<?php
+//att burro
+    $arquivo = fopen ("arquivo.txt", "a");
+
+    $frase1 = $_REQUEST["frase"];
+
+    $frase = fwrite($arquivo, "$frase1 \n");
+    
+    echo "Frase: <br>$frase1<br> salva com sucesso.<br>";
+
+    $frase1 = $_REQUEST["frase"];
+
+    $frase = fwrite($arquivo, "$frase1 \n");
+    
+    echo "Frase: <br>$frase1<br> salva com sucesso.<br>";
---
+++ Aulas 4-5/Exemplo 6/ler_arquivo.php
@@ -0,0 +1,13 @@
+<?php
+
+$arquivo = fopen("arquivo.txt", "r");
+
+// Enquanto o fim do arquivo não for atingido
+while(!feof($arquivo)){
+    // Lê uma linha do arquivo
+    $linha = fgets($arquivo);
+
+    echo "$linha <br>";
+}
+
+fclose($arquivo);
